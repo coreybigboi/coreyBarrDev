431da4159db4bdd05208aeb02f033bafd379d306
Corey Barr <coreybarr01@gmail.com>
2024-02-22 15:13:09 +1100

    added github logo on toobar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,12 +30,14 @@ import {MatTooltipModule} from '@angular/material/tooltip';
 })
 export class AppComponent {
   title = 'coreyBarrDev';
-
+  
+  GITHUB_SVG = '/assets/logos/github/github-mark-white.svg';
+  
   mobileQuery: MediaQueryList;
-
+  
   private _mobileQueryListener: () => void;
-
-  constructor( changeDetectorRef: ChangeDetectorRef ,media: MediaMatcher){
+  
+  constructor( changeDetectorRef: ChangeDetectorRef, media: MediaMatcher){
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
