1d4399459bc6b5f352cc3d064a85b9133bc1e2b5
Corey Barr <coreybarr01@gmail.com>
2024-02-22 14:34:58 +1100

    added snack bar for contact form

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -3,12 +3,16 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input'
 import { MatButtonModule } from '@angular/material/button';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
 import { ContactService } from './contact.service';
+
+const MESSAGE = "Thank you for your message!";
+const ACTION = "Ok";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatSnackBarModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
@@ -20,7 +24,15 @@ export class ContactComponent {
     message: new FormControl('')
   });
 
-  constructor(private contactService: ContactService){}
+  constructor(private contactService: ContactService, private _snackBar: MatSnackBar){}
+
+  private resetForm(){
+    this.contactForm.setValue({
+      name: '',
+      email: '',
+      message: ''
+    });
+  }
 
   submitContactForm(){
     this.contactService.sendMessage(
@@ -28,5 +40,8 @@ export class ContactComponent {
       this.contactForm.value.email ?? '',
       this.contactForm.value.message ?? ''
     );
+
+    this.resetForm();
+    this._snackBar.open(MESSAGE, ACTION);
   }
 }
