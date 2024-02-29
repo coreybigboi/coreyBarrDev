2f3012eb0728e9488cdf09639a431f3745e00b5e
BARR <ITS_BARRCO@health.gov.au>
2024-02-29 15:44:03 +1100

    refactors favourite sounds list on home page to use sound service

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import {FavouriteSoundsService} from "../../shared/services/favourite-sounds.service";
+import {Sound} from "../../shared/models/sound";
+import {NgFor} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  soundsList: Sound[];
 
+  constructor(private soundService: FavouriteSoundsService) {
+    this.soundsList = [];
+  }
+
+  ngOnInit(): void{
+    this.soundService.getSounds().subscribe((data: Sound[]) => {
+      this.soundsList = data;
+    });
+  }
 }
