31dfa68db1abbbcc85148efec5d6c89a0940e57a
BARR <ITS_BARRCO@health.gov.au>
2024-03-06 13:38:00 +1100

    extracts favourite sounds list to its own component

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,31 +1,15 @@
 import { Component } from '@angular/core';
-import {NgForOf} from "@angular/common";
-import {Sound} from "../../shared/models/sound";
-import {NgFor} from "@angular/common";
 import {MatTabsModule} from "@angular/material/tabs";
-import {FavouriteSoundsService} from "../../shared/services/favourite-sounds.service";
-
+import {FavouriteSoundsComponent} from "./favourite-sounds/favourite-sounds.component";
 @Component({
   selector: 'app-about',
   standalone: true,
     imports: [
-        NgForOf,
-        NgFor,
-        MatTabsModule
+        MatTabsModule,
+        FavouriteSoundsComponent
     ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-  soundsList: Sound[];
-
-  constructor(private soundService: FavouriteSoundsService) {
-    this.soundsList = [];
-  }
-
-  ngOnInit(): void{
-    this.soundService.getSounds().subscribe((data: Sound[]) => {
-      this.soundsList = data;
-    });
-  }
 }
---
+++ src/app/pages/about/favourite-sounds/favourite-sounds.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import {Sound} from "../../../shared/models/sound";
+import {NgFor} from "@angular/common";
+import {FavouriteSoundsService} from "../../../shared/services/favourite-sounds.service";
+
+@Component({
+  selector: 'app-favourite-sounds',
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './favourite-sounds.component.html',
+  styleUrl: './favourite-sounds.component.css'
+})
+export class FavouriteSoundsComponent {
+  soundsList: Sound[];
+
+  constructor(private soundService: FavouriteSoundsService) {
+    this.soundsList = [];
+  }
+
+  ngOnInit(): void{
+    this.soundService.getSounds().subscribe((data: Sound[]) => {
+      this.soundsList = data;
+    });
+  }
+}
