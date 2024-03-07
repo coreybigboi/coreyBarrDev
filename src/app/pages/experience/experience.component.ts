20d9d7cd2131f939996fce7036ec02652977850c
coreybigboi <coreybarr01@gmail.com>
2024-03-07 15:50:43 +1100

    adds experience gif

--- src/app/pages/experience/experience.component.ts
+++ src/app/pages/experience/experience.component.ts
@@ -4,15 +4,23 @@ import { ExperienceService } from "../../shared/services/experience.service";
 import { MatCardModule } from '@angular/material/card';
 import { NgFor } from '@angular/common';
 import { SortExperienceByYearPipe } from '../../shared/pipes/sort-experience-by-year.pipe';
+import { ColouredBoxComponent } from '../../shared/components/coloured-box/coloured-box.component';
+import { Colours } from '../../shared/enums/colours';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [MatCardModule, NgFor, SortExperienceByYearPipe],
+  imports: [
+    MatCardModule, 
+    NgFor, 
+    SortExperienceByYearPipe,
+    ColouredBoxComponent
+  ],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css'
 })
 export class ExperienceComponent {
+  blue = Colours.Blue;
   jobs: Job[];
   
   constructor(private experienceService: ExperienceService){
