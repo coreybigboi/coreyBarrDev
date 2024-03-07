8e74e3c65dd170453a8a232809eff08b09b1980e
coreybigboi <coreybarr01@gmail.com>
2024-03-07 17:01:59 +1100

    adds dialogue to about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -3,6 +3,10 @@ import { MatTabsModule } from "@angular/material/tabs";
 import { FavouriteSoundsComponent } from "./favourite-sounds/favourite-sounds.component";
 import { FavouriteMoviesComponent } from "./favourite-movies/favourite-movies.component";
 import { FavouriteGamesComponent } from "./favourite-games/favourite-games.component";
+import { ColouredBoxComponent } from '../../shared/components/coloured-box/coloured-box.component';
+import { AnimatedTextComponent } from '../../shared/components/animated-text/animated-text.component';
+import { Colours } from '../../shared/enums/colours';
+
 @Component({
   selector: 'app-about',
   standalone: true,
@@ -10,10 +14,14 @@ import { FavouriteGamesComponent } from "./favourite-games/favourite-games.compo
         MatTabsModule,
         FavouriteSoundsComponent,
         FavouriteMoviesComponent,
-        FavouriteGamesComponent
+        FavouriteGamesComponent,
+        ColouredBoxComponent,
+        AnimatedTextComponent
     ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  readonly Dialogue = "I don't know what to say about myself, so instead I'll share some of my favourite stuff.";
+  readonly Blue = Colours.Blue;
 }
