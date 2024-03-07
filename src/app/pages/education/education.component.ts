c1ea59b51e6827e06d47f6a5e623126b0459702e
coreybigboi <coreybarr01@gmail.com>
2024-03-07 16:06:03 +1100

    adds education gif

--- src/app/pages/education/education.component.ts
+++ src/app/pages/education/education.component.ts
@@ -7,16 +7,25 @@ import { MatBottomSheet, MatBottomSheetModule } from '@angular/material/bottom-s
 import { EducationDownloadsComponent } from './education-downloads/education-downloads.component';
 import { MatButtonModule } from '@angular/material/button';
 import { SortEducationByDatePipe } from '../../shared/pipes/sort-education-by-date.pipe';
-
+import { ColouredBoxComponent } from '../../shared/components/coloured-box/coloured-box.component';
+import { Colours } from '../../shared/enums/colours';
 
 @Component({
   selector: 'app-education',
   standalone: true,
-  imports: [MatCardModule, NgFor, MatButtonModule, MatBottomSheetModule, SortEducationByDatePipe],
+  imports: [
+    MatCardModule, 
+    NgFor, 
+    MatButtonModule, 
+    MatBottomSheetModule, 
+    SortEducationByDatePipe,
+    ColouredBoxComponent
+  ],
   templateUrl: './education.component.html',
   styleUrl: './education.component.css'
 })
 export class EducationComponent {
+  blue = Colours.Blue;
   educationList: Education[];
 
   constructor(private educationService: EducationService, private _bottomSheet: MatBottomSheet) {
