cff0516c28b3c5e4fd3c53a636b6027823cf8085
coreybigboi <coreybarr01@gmail.com>
2024-03-07 14:48:24 +1100

    removes contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { EducationComponent } from './pages/education/education.component';
 import { ExperienceComponent } from './pages/experience/experience.component';
-import { ContactComponent } from './pages/contact/contact.component';
 import {AboutComponent} from "./pages/about/about.component";
 
 export const routes: Routes = [
@@ -12,5 +11,4 @@ export const routes: Routes = [
     {path: 'projects', component: ProjectsComponent},
     {path: 'education', component: EducationComponent},
     {path: 'experience', component: ExperienceComponent},
-    {path: 'contact', component: ContactComponent},
 ];
