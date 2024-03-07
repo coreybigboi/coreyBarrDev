cdafd8b7568ced2085c6dfdab8156b800c709381
coreybigboi <coreybarr01@gmail.com>
2024-03-08 10:48:55 +1100

    adds loading to projects page

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { ProjectsService } from "../../shared/services/projects.service";
 import { Project } from '../../shared/models/project';
 import { NgFor } from '@angular/common';
+import { NgIf } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBar } from '@angular/material/progress-bar';
 import { SortProjectsByYearPipe } from '../../shared/pipes/sort-projects-by-year.pipe';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [NgFor, MatCardModule, MatButtonModule, SortProjectsByYearPipe],
+  imports: [
+    NgFor, 
+    NgIf,
+    MatCardModule, 
+    MatButtonModule, 
+    MatProgressBar,
+    SortProjectsByYearPipe
+  ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css',
 })
