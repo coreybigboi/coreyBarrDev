31133c4cdd2952d58b83786551579b019703618b
Corey Barr <coreybarr01@gmail.com>
2024-06-28 09:35:41 +1000

    Update favourite sounds

--- src/app/about/ui/favourite-sounds/favourite-sounds.component.ts
+++ src/app/about/ui/favourite-sounds/favourite-sounds.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { NgFor } from "@angular/common";
+import { AsyncPipe, NgFor } from "@angular/common";
 import { NgIf } from '@angular/common';
 import { MatProgressBar } from '@angular/material/progress-bar';
 import { Sound } from "../../../shared/models/sound";
 import { FavouriteSoundsService } from "../../data-access/favourite-sounds.service";
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-favourite-sounds',
@@ -11,21 +12,16 @@ import { FavouriteSoundsService } from "../../data-access/favourite-sounds.servi
   imports: [
     NgFor,
     NgIf,
+    AsyncPipe,
     MatProgressBar
   ],
   templateUrl: './favourite-sounds.component.html',
   styleUrl: './favourite-sounds.component.css'
 })
 export class FavouriteSoundsComponent {
-  soundsList: Sound[];
+  soundsList$: Observable<Sound[]>;
 
   constructor(private soundService: FavouriteSoundsService) {
-    this.soundsList = [];
-  }
-
-  ngOnInit(): void {
-    this.soundService.getSounds().subscribe((data: Sound[]) => {
-      this.soundsList = data;
-    });
+    this.soundsList$ = this.soundService.getSounds(); 
   }
 }
