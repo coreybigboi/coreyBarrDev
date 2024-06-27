ae7bc860db0c995b90c5a0b6dee9df0a81cf4b82
Corey Barr <coreybarr01@gmail.com>
2024-06-28 08:57:58 +1000

    Upgrade to Angular 18

--- src/app/experience/feature/experience.component.ts
+++ src/app/experience/feature/experience.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgFor, NgIf, NgOptimizedImage } from '@angular/common';
+import { AsyncPipe, NgFor, NgIf, NgOptimizedImage } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressBar } from '@angular/material/progress-bar';
 import { ExperienceService } from "../data-access/experience.service";
@@ -9,20 +9,22 @@ import { Job } from '../../shared/models/job';
 import { TerminalTextComponent } from '../../shared/ui/terminal-text/terminal-text.component';
 import { Colours } from '../../shared/enums/colours';
 import { AnimatedTextComponent } from '../../shared/ui/animated-text/animated-text.component';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
   imports: [
-    MatCardModule, 
-    NgFor, 
+    MatCardModule,
+    NgFor,
     NgIf,
-      NgOptimizedImage,
+    NgOptimizedImage,
     SortExperienceByYearPipe,
     ColouredBoxComponent,
     AnimatedTextComponent,
     TerminalTextComponent,
-    MatProgressBar
+    MatProgressBar,
+    AsyncPipe
   ],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css'
@@ -31,16 +33,10 @@ export class ExperienceComponent {
   readonly Dialogue = "Welcome to my job experience page! As you can see, I was a Blackjack dealer long before I wrote my first program and I haven't looked back since!";
   readonly TerminalText = ["Experience"];
   readonly Blue = Colours.Blue;
-  
-  jobs: Job[];
-  
-  constructor(private experienceService: ExperienceService){
-    this.jobs = [];
-  }
 
-  ngOnInit(){
-    this.experienceService.getExperience().subscribe((data: Job[]) => {
-      this.jobs = data;
-    })
+  jobs: Observable<Job[]>;
+
+  constructor(private experienceService: ExperienceService) {
+    this.jobs = this.experienceService.getExperience();
   }
 }
